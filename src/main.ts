e786aa32f0b9ac3793ae37babac8f53fdc9ef6de
Singho9 <136437610+Singho9@users.noreply.github.com>
2023-07-11 11:44:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,16 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+function factorial(n) {
+  // Base case: factorial of 0 or 1 is 1
+  if (n === 0 || n === 1) {
+    return 1;
+  }
 
+ 
+
+  // Recursive case: factorial of n is n multiplied by factorial of (n-1)
+  return n * factorial(n - 1);
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
